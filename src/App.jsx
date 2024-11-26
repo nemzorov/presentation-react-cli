319c720af747f3e58d6094b3087504e64763cadb
techmax <techmax@inbox.ru>
2024-11-26 21:45:00 +0300

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,36 @@ import { useEffect, useState } from 'react';
 import Todo from './Todo';
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
+    setLoading(true);
+
     fetch('https://dummyjson.com/todos')
       .then(res => res.json())
       .then(data => setTodos(data.todos))
   }, [])
 
+  useEffect(() => {
+    if (!todos.length) return;
+    setLoading(false);
+  }, [todos])
+
 
   return (
     <div>
-      <ul>
-        {todos.map((item, index) => (
-          <li key={index}>
-            <Todo todo={item.todo} />
-          </li>
-        ))}
-      </ul>
-
+      {loading ?
+        <span>Загрузка...</span>
+        :
+        <ul>
+          {todos.map((item, index) => (
+            <li key={index}>
+              <Todo todo={item.todo} />
+            </li>
+          ))}
+        </ul>
+      }
     </div>
   )
 }
